2c1ae6ae47bb9dae9b1f9f5869d9e7fc2182820d
Aline Diani <alinediani99@gmail.com>
2024-04-15 23:18:05 -0300

    Adicionar arquivos de projeto.

---
+++ Library.API/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [ApiController]
+    [Route("books")]
+    public class BooksController : Controller
+    {
+        [HttpGet("/get")]
+        public IActionResult GetBook()
+        {
+            return View();
+        }
+
+        [HttpPost("/add")]
+        public IActionResult PostBook()
+        {
+            return View();
+        }
+        [HttpPut("/alter")]
+        public IActionResult PutBook()
+        {
+            return View();
+        }
+        [HttpDelete("/delete")]
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
