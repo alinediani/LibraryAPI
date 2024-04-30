457cdf8b6bd7625b8f262432ebcf861fde8fe7c8
Aline Diani <alinediani99@gmail.com>
2024-04-30 12:23:08 -0300

    Model Creation

---
+++ Library.API/Models/BookModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.API.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public int YearPublication { get; set; }
+    }
+}
---
+++ Library.API/Models/LoanModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.API.Models
+{
+    public class LoanModel
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public int IdBook { get; set; }
+        public string Date { get; set; }
+        public string Email { get; set; }
+    }
+}
